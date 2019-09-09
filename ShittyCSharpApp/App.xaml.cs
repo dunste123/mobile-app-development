69ffb2f2d9e32915e99dc4e77c968a4a2944ce2c
Duncan Sterken <duncan.sterken123@gmail.com>
2019-09-09 16:40:07 +0200

    Stuff

--- ShittyCSharpApp/App.xaml.cs
+++ ShittyCSharpApp/App.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
+using ShittyCSharpApp.Views;
 
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 namespace ShittyCSharpApp
